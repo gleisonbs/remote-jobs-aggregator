d3307d8b32cc9273f506e11eb664d7328c057d1e
Gleison Batista <gleisonbs@gmail.com>
2020-02-09 19:41:17 -0300

    Extract the job title

--- scraper/spiders/flexjobs.py
+++ scraper/spiders/flexjobs.py
@@ -8,25 +8,38 @@ class FlexJobsSpider(Spider):
     def __init__(self):
         self.starting_page = 'jobs/web-software-development-programming'
         self.domain = 'https://www.flexjobs.com'
-        # self.pages_to_visit = set([f'{self.domain}/{self.starting_page}'])
         self.pages_queue = asyncio.Queue()
-
-    def extract_page_feature(self, bs4_soup):
-        print(bs4_soup.title)
-
-    async def update_pages_queue(self, bs4_soup):
-        all_a_tags = bs4_soup.find_all('a', {'class': 'page-link'})
-        for a_tag in all_a_tags:
-            await self.pages_queue.put(f"{self.domain}/{a_tag['href']}")
+        self.pages_already_queued = set()
 
     async def fetch(self, client, url):
         async with client.get(url) as response:
             return await response.text()
 
+
+    async def extract_page_feature(self, bs4_soup):
+        all_job_a_tags = bs4_soup.select('h5 > a')
+        jobs_hrefs = [a_tag['href'] for a_tag in all_job_a_tags]
+
+        for job_href in jobs_hrefs:
+            async with aiohttp.ClientSession() as client:
+                url = f'{self.domain}{job_href}'
+                response = await self.fetch(client, url)
+                job_page = BeautifulSoup(response, 'html.parser')
+                print(job_page.select('div > h1')[0])
+
+
+    async def update_pages_queue(self, bs4_soup):
+        all_a_tags = bs4_soup.find_all('a', {'class': 'page-link'})
+        for a_tag in all_a_tags:
+            url = f"{self.domain}/{a_tag['href']}"
+            if url not in self.pages_already_queued:
+                self.pages_already_queued.add(url)
+                await self.pages_queue.put(url)
+   
+
     async def get_pages(self):
         async with aiohttp.ClientSession() as client:
             while not self.pages_queue.empty():
-                # print(self.pages_queue)
                 url = await self.pages_queue.get()
                 response = await self.fetch(client, url)
                 current_page = BeautifulSoup(response, 'html.parser')
@@ -35,7 +48,8 @@ class FlexJobsSpider(Spider):
                 await self.update_pages_queue(current_page)
 
                 # Extract page elements
-                self.extract_page_feature(current_page)
+                await self.extract_page_feature(current_page)
+
 
     async def run(self):
         await self.pages_queue.put(f'{self.domain}/{self.starting_page}')
